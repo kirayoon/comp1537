ea7d9696a21308683101e150a2c3b60bfe829926
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-11-14 14:44:51 -0800

    Create assignment 2

---
+++ assignment2/index.js
@@ -0,0 +1,48 @@
+var PAGE_SIZE;
+var CURRENT_PAGE = 1;
+
+display_page = function () {
+  $.ajax({
+    url: "https://api.themoviedb.org/3/movie/top_rated?api_key=f0a38761eabbbebf5b14fa1c412ba063",
+    type: "GET",
+    success: function (data) {
+      let start_index = PAGE_SIZE * (CURRENT_PAGE - 1);
+      let end_index = PAGE_SIZE * (CURRENT_PAGE - 1) + PAGE_SIZE;
+      for (i = start_index; i < end_index; i++) {
+        $("main").append(
+          `
+            <div>
+            <h4 style="margin-bottom: 0; margin-top: 5px">${data.results[i].title}</h4>
+            <p style="margin-top: 5px">
+            ${data.results[i].overview}
+            </p>
+            <img src="http://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" 
+            style="width: 100px">
+            <button backdropName="${data.results[i].backdrop_path}" name="${data.results[i].title}" class="backdropBtn">BackDrop Image</button>
+            <hr>
+            </div>
+            `
+        );
+      }
+    },
+  });
+};
+
+setup = function () {
+  $("#search_btn").click(() => {
+    display_page();
+  });
+
+  $("body").on("click", ".backdropBtn", function () {
+    $("#backdropImage").html(
+      `<img src="http://image.tmdb.org/t/p/w500/${$(this).attr(
+        "backdropName"
+      )}">`
+    );
+  });
+  $("body").on("click", ".backdropBtn", function () {
+    $("#backdropTitle").html("BackDrop Image: " + $(this).attr("name"));
+  });
+};
+
+$(document).ready(setup);
