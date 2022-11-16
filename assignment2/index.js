dba0caae12c8c6e875988e4692aded9ab0af54d3
kirayoon <113317993+kirayoon@users.noreply.github.com>
2022-11-16 00:29:51 -0800

    Update code formatting for readability

--- assignment2/index.js
+++ assignment2/index.js
@@ -7,7 +7,7 @@ function displayPageNum(total_pages) {
     $("#pageNum").append(
       `
         <span>
-        <button id="btn${i}" value="${i}">
+        <button class="page_btn" id="btn${i}" value="${i}">
         ${i}
         </button>
         </span>
@@ -18,8 +18,7 @@ function displayPageNum(total_pages) {
 
 function displayPage() {
   // empty page when search button or new page size is clicked
-  $("#movies").empty();
-  $("#pageNum").empty();
+  $("#movies, #pageNum").empty();
 
   // get search term from input
   var movieTitle = $("#searchTerm").val();
@@ -27,38 +26,37 @@ function displayPage() {
     url: `https://api.themoviedb.org/3/search/movie?api_key=f0a38761eabbbebf5b14fa1c412ba063&query=${movieTitle}&language=en-US&page=1&include_adult=false`,
     type: "GET",
     success: function (data) {
-      // set page number buttons
+      // set page numbering
       let result_length = data.results.length;
       let TOTAL_PAGES = Math.ceil(result_length / PAGE_SIZE);
-
-      $("section").css({
-        display: "block",
-      });
+      let start_index = PAGE_SIZE * (CURRENT_PAGE - 1);
+      let end_index = PAGE_SIZE * (CURRENT_PAGE - 1) + Number(PAGE_SIZE);
+      console.log(CURRENT_PAGE, PAGE_SIZE, start_index, end_index);
 
       displayPageNum(TOTAL_PAGES);
 
-      // display first page of movie data
-      let start_index = PAGE_SIZE * (CURRENT_PAGE - 1);
-      let end_index = PAGE_SIZE * (CURRENT_PAGE - 1) + Number(PAGE_SIZE);
-      console.log(CURRENT_PAGE, PAGE_SIZE, end_index);
+      // display movie data
       for (i = start_index; i < end_index; i++) {
         $("#movies").append(
           `
             <div>
-            <h4 style="margin-bottom: 0; margin-top: 5px">
-            #${i + 1} <br> 
-            Title: ${data.results[i].title}</h4>
-            <p style="margin-top: 5px">
-            Description: ${data.results[i].overview}
-            </p>
-            <img src="http://image.tmdb.org/t/p/w500/${
-              data.results[i].poster_path
-            }" 
-            style="width: 100px">
-            <button backdropName="${data.results[i].backdrop_path}" name="${
-            data.results[i].title
-          }" class="backdropBtn">BackDrop Image</button>
-            <hr>
+              <h4 style="margin-bottom: 0; margin-top: 5px">
+              #${i + 1} <br> 
+              Title: ${data.results[i].title}</h4>
+              <p style="margin-top: 5px">
+                Description: ${data.results[i].overview}
+              </p>
+              <img 
+                src="http://image.tmdb.org/t/p/w500/${
+                  data.results[i].poster_path
+                }" 
+                style="width: 100px">
+              <button backdropName="${data.results[i].backdrop_path}" 
+                name="${
+                  data.results[i].title
+                }" class="backdropBtn">BackDrop Image
+              </button>
+              <hr>
             </div>
           `
         );
